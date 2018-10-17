574bb03c7e1a6331c0b8ac509ce377f175f86c3f
maeda <maeda@illi-ichi.com>
2018-10-17 16:55:13 +0900

    add description

--- src/workshop/core.clj
+++ src/workshop/core.clj
@@ -1,153 +1,197 @@
 (ns workshop.core)
 ;; This is a tutorial to learn the basis of making sound with Overtone.
 ;;
-;; Only limited functionality of Overtone are used to make a explanation simple.
 ;; It assumes that you understand basic Clojure syntax.
+;; The use of Overtone functionality is limited as possible to simplify the explanation.
 
 
-;; load overtone first
+;; Load overtone first
 (use 'overtone.live)
 
 ;; sine wave
 ;; ---------
-(comment (image images/sin.png))
-;; play sine wave at 440Hz in 3 sec.
-;; (osc means oscillator)
-(demo 3 (sin-osc 440))
 
-;; change volume by multiplication
-(demo 3 (* 1/2 (sin-osc 440)))
+(comment (image images/sin.png))
+;; Play sine wave at 440Hz in 2 sec.
+;; If frequency is increasing, the sound become higher.
+(demo 2 (sin-osc 440))
+
+;; Change amplitude by multiplication.
+;; Sin wave is ranged from -1 to 1.
+;; If amlitude is decreasing, the volume of sound become smaller.
+(demo 2 (* 1/2 (sin-osc 440)))
 
 ;; CAUTION:
-;; When you evaluate expression below outside 'demo' macro:
+;; Waves arithmetic is supported by Overtone's macro.
+;; When you evaluate the expression below outside of 'demo' macro:
 ;;
 ;; (* 1/2 (sin-osc 440))
 ;;
-;; you will get this error:
+;; You will get this error:
+;;
 ;; Unhandled java.lang.ClassCastException
 ;;    overtone.sc.machinery.ugen.sc_ugen.SCUGen cannot be cast to java.lang.Number
-;;
-;; Waves arithmetic is supported by Overtone's macro.
-
 
 (comment (image images/multiply_sin.png))
-;; change volume over time
-(demo 5 (* (sin-osc 0.4) (sin-osc 440)))
+;; Change amplitude over time.
+;; The volume of sound is changing over time.
+;; If volume changing is more rapid, it will be difficult to recognize volume changing,
+;; then you feel it as changing the timber.
+;; Not only the structure of expression is important, but also the number itself is important.
+(demo 4 (* (sin-osc 0.4) (sin-osc 440)))
 
-;; change frequency over time
-(demo 3 (sin-osc (+ 440 (* 4 (sin-osc 1)))))
+;; Change frequency over time
+;; You would feel the sound is vibrating.
+;; You can increase amount of vibration or make more rapid
+(demo 2 (sin-osc (+ 440 (* 2 (sin-osc 1)))))
 
 ;; you can use local variable
-(demo 3 (let [freq (+ 440 (* 220 (sin-osc 0.2)))]
+(demo 2 (let [freq (+ 440 (* 220 (sin-osc 0.2)))]
           (sin-osc freq)))
 
 ;; sum of sine waves
-(demo 3 (let [freq 440]
-          (->> (range 1 8)
-               (map #(* freq %)) ; integer multiples of the fundamental frequency = harmonic
-               (map sin-osc)
-               (apply +))))
+(demo 2 (let [freq 440]
+          (->>                          ; using threading macro
+           (range 1 8)                  ; make integers from 1 to 7
+           (map #(* freq %))            ; make integer multiples of the fundamental frequency
+                                        ;  = harmonic frequencies
+           (map sin-osc)                ; map frequencies into sin-osc
+           (apply +))))                 ; sum up waves
 
-;; [For users who not familiar with Clojure]
-;; the code above is same as below
+;; --- For non-Clojurians ---
+;; -> or ->> are used to change the order.
 ;;
-;; (safe-demo 3 (let [freq 440]
-;;           (apply + (map sin-osc (map (fn [x] (* freq x)) (range 1 8))))))
+;; The code above is same as below:
+;;
+;; (demo 2 (let [freq 440]
+;;           (apply +
+;;                  (map sin-osc
+;;                       (map (fn [x] (* freq x))
+;;                            (range 1 8))))))
+;; --------------------------------------------
+
+;; harmonic frequencies are essential for the sound of instruments
+;; such as plucking the string or blowing the pipe
+;;
+;; You can read the explanation in detail from:
+;; https://www.soundonsound.com/techniques/synthesizing-wind-instruments
+
 
 ;; clipping
 ;; --------
 
-;; clip signal. The output will be:
+;; Clip signal. The output will be:
 ;;   1 if it's greater than  1
 ;;  -1 if it's greater than -1
-(demo 3 (clip:ar (* 2 (sin-osc 440)) -1 1))
+(demo 2 (->      ; be careful that this is '->' not '->>' in this case
+         (* 2 (sin-osc 440))  ; this expression is ranged from -2 to 2
+         (clip:ar -1 1)))               ; clip over 1 or under -1
 
-;; define macro to avoid the dangerous volume sound
+;; Define macro to avoid the dangerous volume sound
 (defmacro safe-demo [demo-time body]
   `(demo ~demo-time
          (clip:ar ~body -1 1)))
 
-;; now you can use `safe-demo`
-(safe-demo 3 (* 2 (sin-osc 440)))
+;; Now you can use `safe-demo`
+(safe-demo 2 (* 2 (sin-osc 440)))
 
 ;; envelope
 ;; --------
 
 (comment (image images/env_lin.png))
-;; envelope is a line which are concatenated several segments of line
-;; 
+;; Envelope is a line which are concatenated several segments of line
+;;
 ;; (envelope [0 1 0] [0.05 0.5]) expresses a envelope:
 ;;   first increasing from 0 to 1 in 0.05 sec
 ;;   then  decreasing from 1 to 0 in 0.5 sec
 ;; `env-gen` make envelope into wave.
 
 
-;; change volume using a envelope
-(safe-demo 3 (let [freq 880
-                   vol (env-gen (envelope [0 1 0] [0.05 0.5]))]
-               (* vol (sin-osc freq))))
+;; Multiply by envelope
+(safe-demo 2 (let [freq 880
+                   amp (env-gen (envelope [0 1 0] [0.05 0.5]))]
+               (* amp (sin-osc freq))))
 
-;; change frequency using a envelope
-(safe-demo 3 (let [freq (env-gen (envelope [440 880 440 3520] [0.1 0.5 3]))]
-               (sin-osc freq)))
+;; Guess when you hit something.
+;; First, hitting energy change into sound,
+;; then the volume of sound become loud quickly,
+;; then the sound diverge gradually.
 
-;; envelope will be fired when 2nd argument of `env-gen` changes from 1 to 0.
-;; (impulse 1) is trigger at 1Hz
-(safe-demo 3 (let [gate (impulse 1)
-                   freq 880
-                   vol (env-gen (envelope [0 1 0] [0.05 0.5]) gate)]
-               (* vol (sin-osc freq))))
-
+;; The standing duration is smaller, the sound become harder.
+;; The relasing duration is smaller, the sound echo less.
 
 (comment (image images/env_quad.png))
-;; specify line shape of envelope, the third argument of `env-gen`
-;; -4 means the segment curve goes down as x^4
-(safe-demo 3 (let [freq 880
-                   vol (env-gen (envelope [0 1 0] [0.01 0.5] [0 -4]))]
-               (* vol (sin-osc freq))))
+;; Specify line shape of envelope, the third argument of `envelope`
+;; 0 means linear,
+;; positive number means concave down,
+;; negative number means concave up.
+;; The absolute value becomes greater, the curve become more steep.
+(safe-demo 2 (let [freq 880
+                   amp (env-gen (envelope [0 1 0] [0.01 0.5] [0 -4]))]
+               (* amp (sin-osc freq))))
 
 (comment (image images/env_sin.png))
-(safe-demo 3 (let [freq 880
-                   vol (env-gen (envelope [0 1 1 0] [0.2 0.3 0.7] [:sine 0 -4]))]
-               (* vol (sin-osc freq))))
+;; You can use `:sine` to specify the shape.
+;;
+;; The blowing sound needs duration to become stable.
+;; After it get stable, the sound keep going during blowing.
+(safe-demo 2 (let [freq 880
+                   amp (env-gen (envelope [0 1 1 0] [0.2 0.3 0.7] [:sine 0 -4]))]
+               (* amp (sin-osc freq))))
+
+;; You can use a envelope to change frequency.
+;; And envelope has more than two segments.
+(safe-demo 2 (let [freq (env-gen (envelope [440 880 440 3520] [0.1 0.5 3]))]
+               (sin-osc freq)))
 
 (comment (image images/env_step.png))
 ;; example for discontinuous frequency change
-;; :step make immediately jump to final value
-(safe-demo 3 (let [freq (env-gen (envelope [900 900 1300] [0.1 0.5] :step))
-                   vol  (env-gen (envelope [0 1 0] [0 0.6]))]
-               (* vol (sin-osc freq))))
+(safe-demo 2 (let [freq (env-gen (envelope [900 900 1300]
+                                           [0.1 0.5]
+                                           :step)) ; :step make immediately jump to final value
+                   amp  (env-gen (envelope [0 1 0] [0 0.6]))]
+               (* amp (sin-osc freq))))
 
 (comment (image images/env_step_2.png))
 ;; another example
-(safe-demo 3 (let [freq (env-gen (envelope [440.0 440.0 880.0] [0.1 0.4]))
-                   vol  (env-gen (envelope [1 0] [0.5]))]
-               (* vol (sin-osc freq))))
+(safe-demo 2 (let [freq (env-gen (envelope [440.0 440.0 880.0] [0.1 0.4]))
+                   amp  (env-gen (envelope [1 0] [0.5]))]
+               (* amp (sin-osc freq))))
+
+;; fire envelope several times
+(safe-demo 4 (let [gate (impulse 1)     ; this is trigger at 1Hz
+                   freq 880
+                                        ; envelope will be fired when 2nd argument of `env-gen` changes from 1 to 0.
+                   amp (env-gen (envelope [0 1 0] [0.05 0.5]) gate)]
+               (* amp (sin-osc freq))))
+
 
 ;; white-noise and filter
 ;; ---------
 
 ;; (white-noise) generate from -1 to 1 randomly.
 ;; As result, it contains all frequencies
-(safe-demo 3 (white-noise))
+(safe-demo 2 (white-noise))
 
-;; multiply the envelope
-(safe-demo 3 (let [vol (env-gen (envelope [0 1 0] [0.05 0.2] -4))]
-               (* vol (white-noise))))
+;; Multiply the envelope.
+;; The sound is like percussion
+(safe-demo 2 (let [amp (env-gen (envelope [0 1 0] [0.05 0.2] -4))]
+               (* amp (white-noise))))
 
-;; filtering
-(safe-demo 3 (lpf (white-noise) 1000))       ; low pass filter
-(safe-demo 3 (hpf (white-noise) 1000))       ; high pass filter
-(safe-demo 3 (bpf (white-noise) 1000 0.1))   ; band pass filter (if 3rd argument is smaller, the band is narrower)
+;; Filtering
+(safe-demo 2 (lpf (white-noise) 1000))       ; low pass filter
+(safe-demo 2 (hpf (white-noise) 1000))       ; high pass filter
+(safe-demo 2 (bpf (white-noise) 1000 0.1))   ; band pass filter (if 3rd argument is smaller, the band is narrower)
 
-;; resonant low pass filter. The sound is emphasized around 1000Hz
+;; Resonant low pass filter. The sound is emphasized around 1000Hz
 ;; (if 3rd argument is smaller, more emphasizing)
-(safe-demo 3 (rlpf (white-noise) 1000 0.1))
+(safe-demo 2 (rlpf (white-noise) 1000 0.1))
 
-;; change volume and frequency by envelope
-(safe-demo 3 (let [freq (env-gen (envelope [3000 880] [0.025]))
-                   vol  (env-gen (envelope [0 1 0] [0 0.5]))]
-               (* vol (rlpf (white-noise) freq))))
+;; Change amplitude and frequency by envelope
+;; Note that first 0.025 seconds could make big difference.
+(safe-demo 2 (let [freq (env-gen (envelope [3000 800] [0.025]))
+                   amp  (env-gen (envelope [0 1 0] [0 0.5]))]
+               (* amp (rlpf (white-noise) freq))))
 
 
 ;; stereo
@@ -156,21 +200,31 @@
 ;; passing sequence with legth 2,
 ;; first element is used for left channel
 ;; second element is used for right channel
-(safe-demo 3 (map sin-osc [440 660]))
+(safe-demo 2 (map sin-osc [440 660]))
 
 ;; second argment of impulse is phase offset ranged from 0 to 1
-(safe-demo 3 (map #(* (sin-osc %1)
+(safe-demo 2 (map #(* (sin-osc %1)
                       (env-gen (envelope [0 1 0] [0.05 0.5]) (impulse 1 %2)))
                   [440 660]
                   [0 1/2]))
 
-;; Splay spreads an array of channels across the stereo field
-(safe-demo 8 (splay (map #(* 4 (sin-osc %1)
-                             (env-gen (envelope [0 1 0] [0.01 0.5]) (impulse 1/2 %2)))
-                         (->> 400
-                              (iterate #(* 1.4 %))
-                              (take 10))
-                         (range 0 1 0.1))))
+;; another example with streo
+;; the sound is moving around
+(safe-demo 4 (map #(* (sin-osc 440)
+                      (sin-osc 1/6 %)) ; second argment of sin-osc is phase offset ranged from 0 to 2π
+                  [0 (* 1/2 Math/PI)]))
+
+;; `splay' spreads an array of channels across the stereo field
+(safe-demo 8 (splay
+              (map #(* 4 (sin-osc %1)                           ; first argument is for frequency
+                       (env-gen (envelope [0 1 0] [0.01 2] -4)
+                                (impulse 1/2 %2)))              ; second argument is for phase
+
+                   (iterate #(* 1.5 %) 400)                     ; sequence of freqencies
+
+                   (->> (range 0 1 1/10)                        ; sequence of phase
+                        (rotate 1)
+                        reverse))))
 
 ;; exercise
 ;; ---------
@@ -181,15 +235,17 @@
 ;; - closed to real acourstic
 ;; - like strange instruments
 
-;; some example of expressions to complex sound
+;; some examples of expressions to complex sound
 (shuffle (range 100 1000 100))
+
 (take 8 (iterate #(* 1.1 %) 100))       ;don't forget "take".
+
 (->> (reductions * 1000 (cycle [5/4 7/3 11/10 3/5 4/7]))
      (take 20))
 
-(safe-demo (reduce (fn [acc x] (sin-osc (* x acc)))
-                   (sin-osc 10)
-                   (range 1000 5000 1000)))
+(safe-demo 1 (reduce (fn [acc x] (sin-osc (* x acc)))
+                     (sin-osc 10)
+                     (range 1000 5000 1000)))
 
 ;; advanced topics
 ;; ---------
@@ -203,19 +259,19 @@
 
 
 ;; "multichannel expansion" is supported so that the two codes below are the same
-(safe-demo 3 (splay
+(safe-demo 2 (splay
               (map *
                    (map sin-osc (range 500 1500 100))
                    (map #(env-gen (envelope [0 1 0] [0.05 0.5]) (impulse 1 %))
                         (range 0 1 0.1)))))
 
-(safe-demo 3 (splay
+(safe-demo 2 (splay
               (* (sin-osc (range 500 1500 100))
                  (env-gen (envelope [0 1 0] [0.05 0.5]) (impulse 1 (range 0 1 0.1))))))
 
 ;; 'demo' macro release resouces after certain seconds, You can manage the life time of sound.
 ;; In this example, resouce will be released after 0.55 seconds.
-(demo 3 (let [freq (env-gen (envelope [3000 440] [0.1]))
-              vol  (env-gen (envelope [0 1 0] [0.05 0.5]) :action FREE)]
-          (* vol (sin-osc freq))))
+(demo 2 (let [freq (env-gen (envelope [3000 440] [0.1]))
+              amp  (env-gen (envelope [0 1 0] [0.05 0.5]) :action FREE)]
+          (* amp (sin-osc freq))))
 
